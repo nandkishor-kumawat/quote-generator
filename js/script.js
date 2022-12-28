42f43320b275e13e678e3c7eec11425755b7e496
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2022-12-29 01:10:44 +0530

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let generateBtn = document.querySelector("#generateBtn");
-let quote = document.querySelector(".blockquote p");
-let footer = document.querySelector(".blockquote footer");
-
-generateBtn.addEventListener('click', () => {
-    let i = Math.floor(Math.random() * quotes.length);
-    quote.innerHTML = quotes[i].quote;
-    footer.innerHTML = quotes[i].author;
-})
