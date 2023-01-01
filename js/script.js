2703c6e9ae885bf0b0098a1fa2789cd247dd5d8c
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2023-01-02 03:06:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let blockquote = document.querySelector(".blockquote");
 
-function generateQuote() {
+const generateQuote = () => {
     let i = Math.floor(Math.random() * quotes.length);
     blockquote.innerHTML = `<p>${quotes[i].quote}</p><span class="blockquote-footer">${quotes[i].author}</span>`;
 }
+
+document.getElementById('generateBtn').addEventListener('click', generateQuote);
 generateQuote();
