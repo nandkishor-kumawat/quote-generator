6be9bcfbfb5bacf5d502673a4eb47ed6ee090cde
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2023-01-02 18:57:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let spinner = `<div class="lds-spinner"><div></div><div></div><div></div><div></
 
 const generateQuote = async () => {
     blockquote.innerHTML = spinner;
-    let data = await fetch('https://rohsikdnan.github.io/quote-generator/quotes.json');
-    let quotes = await data.json();
+    // let data = await fetch('https://rohsikdnan.github.io/quote-generator/quotes.json');
+    // let quotes = await data.json();
 
     let i = Math.floor(Math.random() * quotes.length);
     blockquote.innerHTML = `<p>${quotes[i].quote}</p><span class="blockquote-footer">${quotes[i].author}</span>`;
