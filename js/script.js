81c7981d6fe9e3b2e0d40c83952ab26deba85c2e
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2023-02-16 19:47:08 +0530

    fixed

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const generateQuote = () => {
     blockquote.innerHTML = spinner;
     // let data = await fetch('https://rohsikdnan.github.io/quote-generator/quotes.json');
     // let quotes = await data.json();
-
     setTimeout(() => {
         let i = Math.floor(Math.random() * quotes.length);
         blockquote.innerHTML = `<p>${quotes[i].quote}</p><span class="blockquote-footer">${quotes[i].author}</span>`;
