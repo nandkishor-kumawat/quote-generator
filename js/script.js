719a3a4715ced1a874224bfb803f7a7fc626769f
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2023-02-16 19:37:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,18 +5,11 @@ const generateQuote = () => {
     blockquote.innerHTML = spinner;
     // let data = await fetch('https://rohsikdnan.github.io/quote-generator/quotes.json');
     // let quotes = await data.json();
-    fetch("quotes.json")
-    .then(res=>res.json())
-    .then(data=>{
-        // setTimeout(() => {
-            let i = Math.floor(Math.random() * data.length);
-            blockquote.innerHTML = `<p>${data[i].quote}</p><span class="blockquote-footer">${data[i].author}</span>`;
-        // }, 500);
-    })
-    // setTimeout(() => {
-    //     let i = Math.floor(Math.random() * quotes.length);
-    //     blockquote.innerHTML = `<p>${quotes[i].quote}</p><span class="blockquote-footer">${quotes[i].author}</span>`;
-    // }, 500);
+
+    setTimeout(() => {
+        let i = Math.floor(Math.random() * quotes.length);
+        blockquote.innerHTML = `<p>${quotes[i].quote}</p><span class="blockquote-footer">${quotes[i].author}</span>`;
+    }, 500);
 }
 
 document.getElementById('generateBtn').addEventListener('click', generateQuote);
